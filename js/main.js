c606011a5e1c7e93313e5c5f4028bc5693d5aca5
dygreen <95523625+dygreen@users.noreply.github.com>
2022-06-24 10:41:11 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   });
 
   // MOBILE) 사이드 메뉴창 열림/닫힘
-  $(".mo_view").css({"left":"-45%"}); /* 처음위치 */
+  $(".mo_view").css({"left":"-65%"}); /* 처음위치 */
   $(".black_back").hide(); /* 검은 배경 숨김 */
 
   /* 메뉴버튼 */
@@ -17,14 +17,14 @@ $(document).ready(function(){
 
   /* 닫기버튼 */
   $(".close_btn a").click(function(){
-    $(".mo_view").stop().animate({"left":"-45%"},500);
+    $(".mo_view").stop().animate({"left":"-65%"},500);
     $(".black_back").fadeOut();
   });
 
   // 검은 배경(모달창)을 클릭하면 닫히는 기능
   document.querySelector(".black_back").addEventListener("click", function(e){
     if(e.target == this){
-      document.querySelector(".mo_view").style.left = "-45%";
+      document.querySelector(".mo_view").style.left = "-65%";
       document.querySelector(".black_back").style.display = "none";
     }
   });
